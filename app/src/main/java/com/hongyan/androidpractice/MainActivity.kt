aaacfe34cd6f76c328fc029dfb32f0e5d2183f30
王宁 <wangninga@rrxplatform.com>
2025-01-13 16:08:30 +0800

    完善

--- app/src/main/java/com/hongyan/androidpractice/MainActivity.kt
+++ app/src/main/java/com/hongyan/androidpractice/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.hongyan.androidpractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import com.hongyan.androidpractice.ui.theme.AndroidPracticeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,20 +37,34 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun jump() {
+        val intent =  Intent(this, ScoreListActivity::class.java)
+        intent.putExtra("key1","Value1")
+        startActivity(intent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidPracticeTheme {
-        Greeting("Android")
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Column {
+            Text(text = "Hello2 $name!", modifier = modifier)
+            Text(text = "哈哈哈哈哈")
+            ImageGroup()
+            Button(onClick = {
+                println("点击了按钮")
+                jump()
+            }) {
+                Icon(imageVector = Icons.Default.Call, contentDescription = null)
+                Text(text = "跳转Button")
+            }
+        }
+    }
+
+    @Composable
+    fun ImageGroup() {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "收拾收拾"
+        )
     }
 }
